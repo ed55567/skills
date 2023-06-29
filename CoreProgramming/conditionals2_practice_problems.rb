64a2e60fe6fce49be05251e90c94cae232f4e0f2
ed55567 <emmanueldaniels37@gmail.com>
2023-06-29 08:15:02 -0400

    made syntax changes to code

--- CoreProgramming/conditionals2_practice_problems.rb
+++ CoreProgramming/conditionals2_practice_problems.rb
@@ -12,43 +12,47 @@ end
 
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
 
-let number = 7;
+number = 7
 
-if (number < 10) {
-  console.log(-1);
-} elseif (number > 10) {
-  console.log(1);
-} else {
-  console.log(0);
-}
- 
+if number < 10
+  puts -1
+elsif number > 10
+  puts 1
+else
+  puts 0
+end
+
 
 
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
-let number1 = 7;
-let number2 = 12;
+number1 = 7
+number2 = 12
 
-if (number1 < 10 && number2 < 10) {
-  console.log(1);
-} else {
-  console.log(0);
-}
+if number1 < 10 && number2 < 10
+  puts 1
+else
+  puts 0
+end
+
 
 
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
 
-let number = 7500;
+number = 7500
 
-if (number > 9000) {
-  console.log(1);
-} else {
-  console.log(-1);
-}
+if number > 9000
+  puts 1
+else
+  puts -1
+end
+
 
 
 # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+
 
 # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
 
