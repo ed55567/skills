25f546597d007101e4961ae2228a11c8bbc66e05
ed55567 <emmanueldaniels37@gmail.com>
2023-06-30 07:07:53 -0400

    worked on conditionals and strings

--- CoreProgramming/conditionals2_practice_problems.rb
+++ CoreProgramming/conditionals2_practice_problems.rb
@@ -52,11 +52,42 @@ end
 
 # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
 
+number = 6
+
+if number < 10
+  puts 9
+elsif number < 20
+  puts 19
+elsif number < 30
+  puts 29
+else
+  puts -1
+end
+
 
 
 # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
 
+number1 = 4
+number2 = 12
+
+if number1 > 10 || number2 > 10
+  puts 100
+else
+  puts -100
+end
+
 # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+year = 1980
+
+if year < 0
+  puts 1776
+else
+  puts 1979
+end
+
+
 
 # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
 
