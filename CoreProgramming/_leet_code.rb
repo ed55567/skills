aa2d2f9ba148e1591b135fd2da79a1df398b6d33
ed55567 <emmanueldaniels37@gmail.com>
2023-07-03 22:53:32 -0400

    condtionals problems 8-10 and leet code

---
+++ CoreProgramming/_leet_code.rb
@@ -0,0 +1,61 @@
+# @param {Integer[]} nums1
+# @param {Integer} m
+# @param {Integer[]} nums2
+# @param {Integer} n
+# @return {Void} Do not return anything, modify nums1 in-place instead.
+# Intuition
+# You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, 
+# and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+# Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+# The final sorted array should not be returned by the function, but instead be 
+# stored inside the array nums1. To accommodate this, nums1 has a length of m + n, 
+# where the first m elements denote the elements that should be merged, and the last 
+# n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+
+def merge(nums1, j, nums2, n)
+    i = nums1.length - 1
+    j -= 1
+    n -= 1
+  
+    while i >= 0 && n >= 0
+      if j >= 0 && nums1[j] > nums2[n]
+        nums1[i] = nums1[j]
+        j -= 1
+      else
+        nums1[i] = nums2[n]
+        n -= 1
+      end
+      i -= 1
+    end
+end
+
+# 27. Remove Element
+# Given an integer array nums and an integer val, 
+# remove all occurrences of val in nums in-place. 
+# The order of the elements may be changed. 
+# Then return the number of elements in nums 
+# which are not equal to val.
+
+# Consider the number of elements in nums which 
+# are not equal to val be k, to get accepted, 
+# you need to do the following things:
+
+# Change the array nums such that the first k 
+# elements of nums contain the elements which 
+# are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+# Return k.
+
+
+def remove_element(nums, val)
+  i = 0
+  nums.each do |num|
+    if num != val
+      nums[i] = num
+      i += 1
+    end
+  end
+  i
+end
--- CoreProgramming/conditionals2_practice_problems.rb
+++ CoreProgramming/conditionals2_practice_problems.rb
@@ -90,7 +90,41 @@ end
 
 
 # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+number = 99
+
+if number == 100
+  puts 100
+elsif number == 99
+  puts 99
+else
+  puts 0
+end
 
 # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
 
+number1 = -5
+number2 = 10
+
+if number1 < 0 && number2 > 0
+  puts 1
+else
+  puts 0
+end
+
+
 # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
+
+number = 55
+
+if number > 80
+  puts 5
+elsif number > 60
+  puts 4
+elsif number > 40
+  puts 3
+elsif number > 20
+  puts 2
+else
+  puts 1
+end
