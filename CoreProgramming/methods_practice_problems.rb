f122956fbc66948c3b128153517a60889a2ff38a
ed55567 <emmanueldaniels37@gmail.com>
2023-07-06 12:27:48 -0400

    added methods 1-5 questions

--- CoreProgramming/methods_practice_problems.rb
+++ CoreProgramming/methods_practice_problems.rb
@@ -1,12 +1,49 @@
 # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
+print "Enter a word: "
+word = gets.chomp
+
+capitalized_word = word.upcase
+puts capitalized_word
+
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+print "Enter a number: "
+number = gets.chomp.to_i
+
+if number > 100
+  puts "That's a big number"
+end
+
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
+print "Enter the first number: "
+number1 = gets.chomp.to_i
+
+print "Enter the second number: "
+number2 = gets.chomp.to_i
+
+sum = number1 + number2
+puts "The sum is: #{sum}"
+
+
 # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
 
+print "Enter a word: "
+word = gets.chomp
+
+puts word.reverse
+
+
 # 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+print "Enter a number: "
+number = gets.chomp.to_i
+
+result = number * 10
+puts "The result is: #{result}"
+
 
 # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
 
